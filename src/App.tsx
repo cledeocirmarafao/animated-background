043b5ea39bfcc55dded57921fcac86b9565d2cd0
Cledeocir Marafão <cledeocirms96@gmail.com>
2026-01-02 07:49:42 -0300

    Feat: Section Preview relocated to within the Main

--- src/App.tsx
+++ src/App.tsx
@@ -33,8 +33,8 @@ function App() {
       />
       <Main onBackgroundGenerated={handleBackgroundGenerated}>
         <CodeOutput htmlCode={backgroundHtml} cssCode={backgroundCss}/>
-      </Main>
       <Preview />
+      </Main>
     </>
   );
 }
